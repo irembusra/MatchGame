48b43a531234d47f24d0912e05e49f4663aa119b
İrem Büşra KILIÇ <37072323+irembusra@users.noreply.github.com>
2018-10-22 16:59:47 +0300

    Piece Color

---
+++ Match2D/Assets/Piece.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Piece : MonoBehaviour {
+
+    // colors
+    private Color[] colors = new Color[6]{
+        Color.red,
+        Color.blue,
+        Color.green,
+        Color.white,
+        Color.yellow,
+        Color.magenta
+
+    };
+    public SpriteRenderer sprite;
+
+    public int index;
+    public Vector2 coordinates;
+    public bool destroyed;
+	// Use this for initialization
+	void Start () {
+
+        index = Random.Range(0, colors.Length);
+        sprite.color = colors[index];
+	}
+    public bool IsNeighbour(Piece otherPiece)
+    {
+        return Mathf.Abs(otherPiece.coordinates.x - this.coordinates.x) + Mathf.Abs(otherPiece.coordinates.y - this.coordinates.y) == 1;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
